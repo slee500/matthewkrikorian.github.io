7ab51b54d3e3df55760ee14e299f77258665f2c2
Matthew Krikorian <matthewkrikorian@gmail.com>
2017-12-06 21:43:08 -0600

    tweaks

--- app.js
+++ app.js
@@ -1,45 +1,40 @@
 typer('body')
   .line()
-  .line('<span style="color: #df005e">krikorian</span><span style="color: #e0d55a">@home</span>:</span><span style="color: #53d1ed">~/site</span> $ </span>', 0)
+  .line('[<span style="color: #df005e">krikorian</span><span style="color: #e0d55a">@home</span>:</span><span style="color: #53d1ed">~/site</span>]$ </span>', 0)
   .pause(750)
 
   // cd ~/
   .continue('cd ~/site', 100)
   .line()
-  .line('<span style="color: #df005e">krikorian</span><span style="color: #e0d55a">@home</span>:</span><span style="color: #53d1ed">~/site</span> $ </span>', 0)
+  .line('[<span style="color: #df005e">krikorian</span><span style="color: #e0d55a">@home</span>:</span><span style="color: #53d1ed">~/site</span>]$ </span>', 0)
   .pause(750)
 
   // ls -al
-  .continue('ls -al', 100)
+  .continue('la', 100)
   .line()
   //.line('total 8', 0)
   .line('total 7', 0)
-  .line('drwxr-xr-x   14 krikorian  staff      476 Jul 20 16:09 .secretstuff', 0)
-  .line('-rw-r--r--    1 krikorian  staff    61572 Mar  4 02:07 aboutme.txt', 0)
-  .line('-rw-r--r--    1 krikorian  staff      552 Apr 27 13:27 contactme.txt', 0)
-  .line('-rw-r--r--    1 krikorian  staff       37 Aug  6 23:09 futureplans.txt', 0)
-  .line('-rw-r--r--    1 krikorian  staff      327 Aug  8 23:47 profexperience.txt', 0)
-  .line('lrw-------    1 krikorian  staff     7354 Aug  6 23:09 resume -> <a href="MatthewKrikorianResume.pdf">/etc/resume.pdf</a>', 0)
+  .line('-rw-r--r--@   1 krikorian  staff     7354 Dec  6 23:09 resume -> <a href="MatthewKrikorianResume.pdf">/etc/resume.pdf</a>', 0)
+  .line('-rw-r--r--    1 krikorian  staff    61572 Dec  4 02:07 aboutme.txt', 0)
+  .line('-rw-r--r--    1 krikorian  staff    61572 Dec  4 02:07 education.txt', 0)
+  .line('-rw-r--r--    1 krikorian  staff      552 Dec  6 13:27 findme.txt', 0)
+  .line('-rw-r--r--    1 krikorian  staff      327 Dec  6 23:47 experience.txt', 0)
   .line()
-  .line('<span style="color: #df005e">krikorian</span><span style="color: #e0d55a">@home</span>:</span><span style="color: #53d1ed">~/site</span> $ </span>', 0)
+  .line('[<span style="color: #df005e">krikorian</span><span style="color: #e0d55a">@home</span>:</span><span style="color: #53d1ed">~/site</span>]$ </span>', 0)
   .pause(1500) // long wait while you look at items in dir
 
-  // cat aboutme.txt
   .continue('cat a', 100)
   .continue('boutme.txt ', 0) // tab completion
   .line()
   .line('Hi, I&apos;m Matthew!', 0)
   .line()
-  .line('I&apos;m an undergrad student at the University of Illinois at Urbana Champaign studying Computer Engineering. My interests are in Artificial Intelligence and Human-Computer Interaction. I&apos;m currently a junior and will be interning at ViaSat in the Summer.', 0)
+  .line('I&apos;m an undergraduate student at the University of Illinois at Urbana Champaign studying Computer Engineering. My interests lie in both the technology and finance, and would keep up with both very avidly. I&apos;m currently a junior and will be interning at ViaSat in the Summer.', 0)
   .line()
-  .line('<span style="color: #df005e">krikorian</span><span style="color: #e0d55a">@home</span>:</span><span style="color: #53d1ed">~/site</span> $ </span>', 0)
+  .line('[<span style="color: #df005e">krikorian</span><span style="color: #e0d55a">@home</span>:</span><span style="color: #53d1ed">~/site</span>]$ </span>', 0)
   .pause(1250)
 
-  // cat profexperience.txt && cat futureplans.txt
-  .continue('cat p', 100)
-  .continue('rofexperience.txt ', 0) // tab completion
-  .continue('&& cat f', 100)
-  .continue('utureplans.txt ', 0) // tab completion
+  .continue('cat e', 100)
+  .continue('xperience.txt ', 0) // tab completion
   .line()
   .line('Summer 2018: Incoming Software Engineering Intern at ViaSat', 0)
   .line()
@@ -47,7 +42,7 @@ typer('body')
   .line()
   .line('Summer 2016: Undergraduate Research Assistant at UIUC',0)
   .line()
-  .line('<span style="color: #df005e">krikorian</span><span style="color: #e0d55a">@home</span>:</span><span style="color: #53d1ed">~/site</span> $ </span>', 0)
+  .line('[<span style="color: #df005e">krikorian</span><span style="color: #e0d55a">@home</span>:</span><span style="color: #53d1ed">~/site</span>]$ </span>', 0)
   .pause(1250)
 
   // cat contactme.txt
@@ -61,7 +56,7 @@ typer('body')
   .line('<a href="https://www.linkedin.com/in/mattkrikorian/"><img src="linkedin.png" alt="LinkedIn" height="21" width="21"></a>', 0)
   .continue(' <a href="https://github.com/matthewkrikorian"><img src="github.png" alt="GitHub" height="21" width="21"></a>', 0)
   .line()
-  .line('<span style="color: #df005e">krikorian</span><span style="color: #e0d55a">@home</span>:</span><span style="color: #53d1ed">~/site</span> $ </span>', 0)
+  .line('[<span style="color: #df005e">krikorian</span><span style="color: #e0d55a">@home</span>:</span><span style="color: #53d1ed">~/site</span>]$ </span>', 0)
   .pause(1250)
 
   // sudo shutdown -h now
@@ -72,7 +67,3 @@ typer('body')
 
   .cursor({block: true, blink: 'hard', color: '#f6f7ee'})
 ;
-
-// use http://manytools.org/image/colorize-filter/ to color icons
-
-// docs: https://www.npmjs.com/package/typer-js
